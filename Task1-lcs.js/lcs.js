6315f5a983671d34da86930fbc9a7a57f52e497f
RapidRapidRabbit <awestruck31@mail.ru>
2022-09-18 17:45:08 +0300

    task 1 fix, task 2

--- Task1-lcs.js/lcs.js
+++ Task1-lcs.js/lcs.js
@@ -1,1 +1,1 @@
-lcs = (a) => {if(!a.length)return '';let m=a.reduce((a,b)=>a.length<=b.length?a:b),x=m.length;for(let l=x;l>0;l--){for(let s=0;s<=x-l;s++){let r=m.slice(s,l);if(a.every(e=>e.includes(r)))return r;}}return '';};console.log(lcs(process.argv.slice(2)));
+lcs = (a) => {if(!a.length)return '';let m=a.reduce((a,b)=>a.length<=b.length?a:b),x=m.length;for(let l=x;l>0;l--){for(let s=0;s<=x-l;s++){let r=m.slice(s,s+l);if(a.every(e=>e.includes(r)))return r;}}return '';};console.log(lcs(process.argv.slice(2)));
---
+++ Task2-SHA3-256/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace Task2_SHA3_256
+{
+    public class Program
+    {
+        private const string Email = "ddubina.job@gmail.com";
+        private static readonly DirectoryInfo _directory = new("D:\\git\\ITransition-Tasks\\Task2-SHA3-256\\DataFiles");
+        private static readonly Sha3Digest _hashAlgorithm = new(256);
+        
+        static void Main(string[] args)
+        {
+            var files = _directory.GetFiles();
+            var hashes = new List<string>();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                var fileData = GetFileData(files[i]);
+
+                string hashString = GetHashString(fileData);
+
+                hashes.Add(hashString);
+
+                Console.WriteLine($"#{i + 1} '{files[i].Name}' SizeOK: {files[i].Length == fileData.Length}  Result: {hashString}");
+            }
+
+            hashes.Sort();
+            hashes.Add(Email);
+
+            var hashesString = string.Join("", hashes);
+
+            Console.WriteLine($"Result: {GetHashString(Encoding.ASCII.GetBytes(hashesString))}");
+        }
+
+        private static byte[] GetFileData(FileInfo fileInfo)
+        {
+            using var fileStream = fileInfo.Open(FileMode.Open);
+            var bytesToRead = fileStream.Length;
+            var result = new byte[bytesToRead];
+
+            int numBytesRead = 0;
+
+            while (bytesToRead > 0)
+            {
+                int n = fileStream.Read(result, numBytesRead, (int)bytesToRead);
+
+                if (n == 0)
+                    break;
+
+                numBytesRead += n;
+                bytesToRead -= n;
+            }
+
+            return result;
+        }
+
+        private static string GetHashString(byte[] data)
+        {
+            byte[] hash = new byte[32];
+
+            _hashAlgorithm.BlockUpdate(data, 0, data.Length);
+            _hashAlgorithm.DoFinal(hash, 0);
+
+            return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+        }
+    }
+}
